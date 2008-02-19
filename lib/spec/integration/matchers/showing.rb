711ce0bd3c9de89f2a8b2331f6785caa3082d8af
Adam Williams <adam@thewilliams.ws>
2008-02-19 16:08:48 +0000

    spec_integration: be_showing path disreguards query parameters in actual path unless expected includes them

--- lib/spec/integration/matchers/showing.rb
+++ lib/spec/integration/matchers/showing.rb
@@ -8,6 +8,12 @@ module Spec
         
         def matches?(response)
           @actual = response.request.request_uri
+          actual_path, query_params_in_actual = @actual.split('?')
+          expected_path, query_params_in_expected = @expected.split('?')
+          if !query_params_in_expected
+            @actual = actual_path
+            @expected = expected_path
+          end
           @actual == @expected
         end
         
@@ -25,6 +31,11 @@ module Spec
       # When writing integration tests, whether a ton of redirects happen or
       # not isn't important. You want to be sure that a particular path is
       # being shown.
+      #
+      # This will not attempt to match query parameters unless you provide
+      # them in your path ('my/path?a=b'). Why? Because sometimes you don't
+      # care what the query parameters are!
+      #
       def be_showing(path)
         Showing.new(path)
       end
