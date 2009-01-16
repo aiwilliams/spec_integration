199f9c9382f72c371dd0ec9a1bf44b9e980ace07
Alf Mikula <amikula@Kale.(none)>
2009-01-15 20:45:33 -0800

    Don't alias rescue_action_without_fast_errors...instead call it directly when in an integration example

--- lib/spec/integration/extensions/action_controller.rb
+++ lib/spec/integration/extensions/action_controller.rb
@@ -3,8 +3,6 @@
 # that pass through exceptions (see “Expecting Errors” on
 # http://rspec.info/rdoc-rails/classes/Spec/Rails/Example/ControllerExampleGroup.html)
 ActionController::Base.class_eval do
-  alias_method :rescue_action, :rescue_action_without_fast_errors
-  
   def use_rails_error_handling_with_integration_support?
     if Spec::Integration::DSL::IntegrationExample::during_integration_example
       true
@@ -16,8 +14,12 @@ ActionController::Base.class_eval do
   
   attr_reader :rescued_exception
   def rescue_action_with_integration_support(e)
-    @rescued_exception = e
-    rescue_action_without_integration_support e
+    if Spec::Integration::DSL::IntegrationExample::during_integration_example
+      @rescued_exception = e
+      rescue_action_without_fast_errors e
+    else
+      rescue_action_without_integration_support e
+    end
   end
   alias_method_chain :rescue_action, :integration_support
 end
